ae538de5fa576b92a91496a7e9d2a382ef785e97
drzl777 <andrew.zheng725@gmail.com>
2017-06-02 10:36:50 -0400

    Done.

--- app/controllers/appearances_controller.rb
+++ app/controllers/appearances_controller.rb
@@ -1,5 +1,7 @@
 class AppearancesController < ApplicationController
 
+  before_action :authenticate
+
   def new
     @appearance = Appearance.new
   end
@@ -13,10 +15,27 @@ class AppearancesController < ApplicationController
     end
   end
 
+  def show
+    @appearance = Appearance.find(params[:id])
+  end
+
+  def edit
+    @appearance = Appearance.find(params[:id])
+  end
+
+  def update
+    @appearance = Appearance.find(params[:id])
+    if @appearance.update(appearance_params)
+      redirect_to @appearance.episode
+    else
+      redirect_to edit_appearance_path
+    end
+  end
+
   private
 
   def appearance_params
-    params.require(:appearance).permit(:guest_id, :episode_id, :rating)
+    params.require(:appearance).permit(:guest_id, :episode_id, :rating, :user_id)
   end
 
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_player
+  helper_method :logged_in?
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def authenticate
+    unless logged_in?
+      flash[:danger] = "Please Log In to perform this action"
+      redirect_to episodes_path
+    end
+  end
 end
